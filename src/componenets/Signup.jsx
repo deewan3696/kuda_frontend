ec40f5b20070f5f4b08c2cd83e3c60d3ee11295e
deewan3696 <mail_d1@yahoo.com>
2022-11-23 10:33:37 +0100

    updated signup file

--- src/componenets/Signup.jsx
+++ src/componenets/Signup.jsx
@@ -2,6 +2,7 @@
 import "../css/index.css"
 import Logo from '../images/Logo.svg'
 import Unhidepw from '../images/Unhidepw.svg'
+//import Logo2 from '../images/Logo.svg'
 
 
 
@@ -19,21 +20,25 @@ function Signup() {
                 </div>
             </nav>
             <div>
-            <div className="LinkOntopForm rounded my-5 fw-bold">Please check that you are visiting the correct URL <a className="theLink fw-bold" href='https://app.kuda.com/'>https://app.kuda.com</a></div>
-                <div className="wholeTable">
+            <div className="CheckUrl rounded my-5 fw-bold">Please check that you are visiting the correct URL <a className="anchorLink fw-bold" href='https://app.kuda.com/'>https://app.kuda.com</a></div>
+                <div className="kudaCont">
                     <h4 className="fw-bold">Sign In to Kuda</h4>
-                    <p>
+                    <p id="para">
                         To sign in, please type in the email address linked to <br/> your Kuda account.
                     </p>
                     <form>
                         <label htmlFor='email Address'>Email Address</label><br/>
-                        <input type="text" placeholder="example@gmail.com"/><br/><br/>
+                        <input id="kudaInput1" type="text" placeholder="example@gmail.com"/><br/><br/>
                         <label htmlFor='Passowrd'>Passowrd</label><br/>
-                        <input type="password" placeholder="••••••••••" /><img src={Unhidepw } alt="Bootstrap" width="20" height="20"/><br/><br/>
-                        <p className='paragraphpasswordreset'>Forgot Password?<a className='passwordReset' href='https://app.kuda.com/reset'>Reset it</a></p>
+                        <input id="kudaInput1" type="password" placeholder="------------" /><img src={Unhidepw } alt="Bootstrap" width="20" height="20"/><br/><br/>
+                        <p className='resetPw'  id="para">Forgot Password?<a className='pwordReset' href='https://app.kuda.com/reset'>Reset it</a></p>
                     </form>
+                  <button className="kuda-btn">Sign In</button>
+                  <p className="p-text">if you dont have a kuda account,download the app  <br /> <a href="https://bit.ly/getkuda">here</a>  and open an account in a few minutes </p>
                 </div>
             </div>
+         
+        
         </section>
        
         
